2e6319420cac76490de06bfd5e826be3a111d943
Nazmus Sakib <sakibn916@gmail.com>
2026-06-26 20:41:40 +0600

    commiting with labTask1

--- src/donor/donor.controller.ts
+++ src/donor/donor.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
+import { DonorService } from './donor.service';
 
 @Controller('donor')
-export class DonorController {}
+export class DonorController {
+
+    constructor(private readonly donorService: DonorService) {}
+    @Get('crises')
+  getCrises(@Query('type') type: string, @Query('city') city: string): object {
+    return this.donorService.getCrises(type, city);
+  }
+
+  @Get('crises/:id')
+  getCrisisById(@Param('id',ParseIntPipe) id:number):object{
+    console.log(id);
+    console.log(typeof id);
+
+    return this.donorService.getCrisisById(id);
+  }
+
+  /*@Get('crises/:id')
+  getCrisisById(@Param('id') id: number): object {
+    return this.donorService.getCrisisById(id);
+  }*/
+
+  @Get('donations')
+  getMyDonations(@Query('status') status: string): object {
+    return this.donorService.getMyDonations(status);
+  }
+
+  @Get('donations/:id')
+  getDonationById(@Param('id') id: number): object {
+    return this.donorService.getDonationById(id);
+  }
+
+  @Get('test')
+test() {
+  return {
+    message: 'Test route works',
+  };
+}
+
+}
--- src/donor/donor.service.ts
+++ src/donor/donor.service.ts
@@ -1,4 +1,53 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class DonorService {}
+export class DonorService {
+    getCrises(type: string, city: string): object {
+    return {
+      message: "Crises fetched successfully",
+      data: [
+        { id: 1, title: "Flood Relief - Sylhet", fundsNeeded: 100000, fulfilled: 65000 },
+        { id: 2, title: "Cyclone Recovery - Cox's Bazar", fundsNeeded: 50000, fulfilled: 12000 }
+      ]
+    }
+  }
+
+  getCrisisById(id: number): object {
+    return {
+      message: "Crisis fetched successfully",
+      data: {
+        id: id,
+        title: "Flood Relief - Sylhet",
+        fundsNeeded: 100000,
+        fulfilled: 65000,
+        resourceRequests: [
+          { item: "Clean Water", quantityNeeded: 500 },
+          { item: "Cash Donation", quantityNeeded: 35000 }
+        ]
+      }
+    }
+  }
+
+  getMyDonations(status: string): object {
+    return {
+      message: "Donation history fetched successfully",
+      data: [
+        { id: 1, crisisName: "Flood Relief - Sylhet", amount: 5000, status: "received" },
+        { id: 2, crisisName: "Cyclone Recovery - Cox's Bazar", amount: 2000, status: "pledged" }
+      ]
+    }
+  }
+
+  getDonationById(id: number): object {
+    return {
+      message: "Donation fetched successfully",
+      data: {
+        id: id,
+        crisisName: "Flood Relief - Sylhet",
+        amount: 5000,
+        status: "received"
+      }
+    }
+  }
+}
+
